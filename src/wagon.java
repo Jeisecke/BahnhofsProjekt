87a9fb41f691742493e767ce25c2bc1de05bf3d5
Jakob Jeisecke <jakob.jeisecke@icloud.com>
2025-10-29 12:55:48 +0100

    done

--- src/wagon.java
+++ src/wagon.java
@@ -1,8 +1,10 @@
 package bahnhofverwaltung;
 
 public class wagon {
-
-    public wagon() {
-
+    String ladung;
+    int id;
+    public wagon(String ladung, int id) {
+        this.ladung = ladung;
+        this.id = id;
     }
 }
