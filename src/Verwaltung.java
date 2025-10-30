5b24d6b06d94e143aa17b0237e91d15080683df4
Onur Ertas <onur@Laptop-Onur.192.168.0.1>
2025-10-30 10:38:54 +0100

    verw. ausgebaut

--- src/Verwaltung.java
+++ src/Verwaltung.java
@@ -3,10 +3,43 @@ package bahnhofverwaltung;
 
 public class Verwaltung {
 
-    private Stack gleis1;
-    private Stack gleis2;
-    private Stack gleis3;
+    private Stack<Wagon> gleis1;
+    private Stack<Wagon> gleis2;
+    private Stack<Wagon> gleis3;
+
+    private int anzahlZüge;
 
     private int[] idList;
-    
+
+    public void Verwaltung(int anzahlZüge)
+    {
+        this.anzahlZüge = anzahlZüge;
+
+        gleis1 = new Stack<Wagon>();
+        gleis2 = new Stack<Wagon>();
+        gleis3 = new Stack<Wagon>();
+
+        idList = new int[anzahlZüge]; //temporärer Wert! Bitte noch ändern!
+        
+
+    }
+
+    public void schieneWechseln(int von, int zu)
+    {
+
+    }
+
+
+
+    private void zugHinzufügen(int zugGröße, int target)
+    {
+        final int zugId = idGenerieren();
+        target.push(new Wagon(zugGröße, zugId));
+        idList[zugId] = zugId;
+    }
+
+    private int idGenerieren();
+    {
+        return(0);
+    }
 }
