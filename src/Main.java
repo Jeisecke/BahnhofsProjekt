f0422f3b0d19d8b38a3f3c247aaf65ec92e400ed
Jakob Jeisecke <jakob.jeisecke@icloud.com>
2025-10-30 10:57:56 +0100

    Reduced spaghetti in main java

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ package bahnhofverwaltung;
 
 
 public class Main {
-    public static void Main(String[] args) {
+    public void Main() {
         Verwaltung verwaltung;
         verwaltung = new Verwaltung(10);
     }
